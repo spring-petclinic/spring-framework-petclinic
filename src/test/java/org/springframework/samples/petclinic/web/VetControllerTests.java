f366df3f5a93e0f73af838b959720c5a311128cf
Antoine Rey <antoine.rey@gmail.com>
2019-10-10 10:22:14 +0200

    Upgrade to Spring Framework 5.2.0

--- src/test/java/org/springframework/samples/petclinic/web/VetControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/VetControllerTests.java
@@ -63,7 +63,7 @@ public class VetControllerTests {
     public void testShowResourcesVetList() throws Exception {
         ResultActions actions = mockMvc.perform(get("/vets.json").accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk());
-        actions.andExpect(content().contentType("application/json;charset=UTF-8"))
+        actions.andExpect(content().contentType("application/json"))
             .andExpect(jsonPath("$.vetList[0].id").value(1));
     }
 
