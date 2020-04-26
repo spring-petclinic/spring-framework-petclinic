9528000794d93049b23125f70b9b9b4a14ac924f
Antoine Rey <antoine.rey@gmail.com>
2020-04-26 10:29:48 +0200

    Add private constructor

--- src/main/java/org/springframework/samples/petclinic/util/EntityUtils.java
+++ src/main/java/org/springframework/samples/petclinic/util/EntityUtils.java
@@ -30,7 +30,11 @@ import org.springframework.samples.petclinic.model.BaseEntity;
  * @see org.springframework.samples.petclinic.model.BaseEntity
  * @since 29.10.2003
  */
-public abstract class EntityUtils {
+public class EntityUtils {
+
+    private EntityUtils() {
+        // Utility class
+    }
 
     /**
      * Look up the entity of the given class with the given id in the given collection.
@@ -41,8 +45,7 @@ public abstract class EntityUtils {
      * @return the found entity
      * @throws ObjectRetrievalFailureException if the entity was not found
      */
-    public static <T extends BaseEntity> T getById(Collection<T> entities, Class<T> entityClass, int entityId)
-        throws ObjectRetrievalFailureException {
+    public static <T extends BaseEntity> T getById(Collection<T> entities, Class<T> entityClass, int entityId) {
         for (T entity : entities) {
             if (entity.getId() == entityId && entityClass.isInstance(entity)) {
                 return entity;
